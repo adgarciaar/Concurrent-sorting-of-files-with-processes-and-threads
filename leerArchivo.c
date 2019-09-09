b449a25d55518763578fe5ff4e399c21c221207d
Adrian Garcia <33211027+adgarciaar@users.noreply.github.com>
2019-09-09 12:58:00 -0500

    cambio de nombres acorde a estiloC

--- leerArchivo.c
+++ leerArchivo.c
@@ -1,38 +1,38 @@
+#include "leerArchivo.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "leerArchivo.h"
 
-int ContarLineasArchivo(char filename[]);
-void OrdenarRegistro(registro* record, int numeroElementos);
-void ImprimirArchivoTemporal(registro* record, int numeroElementos, char filename[]);
+int ContarLineasArchivo(char nombre_archivo[]);
+void OrdenarRegistro(registro* array_registros, int numero_elementos);
+void ImprimirArchivoTemporal(registro* array_registros, int numero_elementos, char nombre_archivo[]);
 
 int main (int argc, char **argv) {
 
     FILE * archivo;
 
-    char linea[maxTamanoCadena];
-    char lineaAux[maxTamanoCadena];
+    char linea[max_tamano_cadena];
+    char linea_aux[max_tamano_cadena];
 
     char delim[] = " ";
     char delim1[] = "\n";
     int contador = 0;
-    int numeroLinea = 0;
+    int numero_linea = 0;
     int i = 0;
     char *item;
     char *ptr;
 
-    int numeroLineasArchivo = ContarLineasArchivo(argv[1]);
+    int numero_lineasArchivo = ContarLineasArchivo(argv[1]);
 
-    /*registro record[numeroLineasArchivo];*/
-    registro* record = (registro*)malloc(numeroLineasArchivo*sizeof(registro));
+    /*registro array_registros[numero_lineasArchivo];*/
+    registro* array_registros = (registro*)malloc(numero_lineasArchivo*sizeof(registro));
 
     if(argc!=2){
         printf("Error con numero de argumentos\n");
         exit(1);
     }
 
-    if (record == NULL) {
+    if (array_registros == NULL) {
         printf("Memory not allocated.\n");
         exit(1);
     }
@@ -43,9 +43,9 @@ int main (int argc, char **argv) {
       /* Read file line by line */
 
       while (fgets(linea,sizeof(linea),archivo)) {
-          strcpy(record[numeroLinea].cadena,linea);
-          /*printf("%s",record[numeroLinea].cadena);*/
-          numeroLinea = numeroLinea + 1;
+          strcpy(array_registros[numero_linea].cadena,linea);
+          /*printf("%s",array_registros[numero_linea].cadena);*/
+          numero_linea = numero_linea + 1;
       }
       fclose(archivo);/* Close file */
 
@@ -54,29 +54,29 @@ int main (int argc, char **argv) {
     }
 
 
-    for (i = 0; i < numeroLineasArchivo; i++){
+    for (i = 0; i < numero_lineasArchivo; i++){
 
-        strcpy(lineaAux, record[i].cadena);
+        strcpy(linea_aux, array_registros[i].cadena);
 
-        if( (ptr = strchr(lineaAux, '\n')) != NULL)
+        if( (ptr = strchr(linea_aux, '\n')) != NULL)
           *ptr = '\0';
 
-        item = strtok(lineaAux, delim);
+        item = strtok(linea_aux, delim);
         contador = 1;
         while(item != NULL){
             /*printf("Contador: %d\n",contador);
             printf("%s\n",item);*/
             if (contador == 4){
-                record[i].tiempoEjecucion = atoi(item);
-                /*printf("%d\n",record[i].tiempoEjecucion);*/
+                array_registros[i].tiempo_ejecucion = atoi(item);
+                /*printf("%d\n",array_registros[i].tiempo_ejecucion);*/
             }
             if (contador == 5){
-                strcpy(record[i].fechaEjecucion,item);
-                /*printf("%s\n",record[i].fechaEjecucion);*/
+                strcpy(array_registros[i].fecha_ejecucion,item);
+                /*printf("%s\n",array_registros[i].fecha_ejecucion);*/
             }
             if (contador == 6){
-                strcpy(record[i].horaEjecucion,item);
-                /*printf("%s\n",record[i].horaEjecucion);*/
+                strcpy(array_registros[i].hora_ejecucion,item);
+                /*printf("%s\n",array_registros[i].hora_ejecucion);*/
                 item = NULL;
             }
 
@@ -86,37 +86,37 @@ int main (int argc, char **argv) {
 
     }
 
-    OrdenarRegistro(record, numeroLineasArchivo);
+    OrdenarRegistro(array_registros, numero_lineasArchivo);
 
     /*i = 0;
-    for (i = 0; i < numeroLineasArchivo; i++){
+    for (i = 0; i < numero_lineasArchivo; i++){
       printf("Contador:%d\n",i);
-      printf("%s",record[i].cadena);
-      printf("%d ",record[i].tiempoEjecucion);
-      printf("%s ",record[i].fechaEjecucion);
-      printf("%s\n",record[i].horaEjecucion);
+      printf("%s",array_registros[i].cadena);
+      printf("%d ",array_registros[i].tiempo_ejecucion);
+      printf("%s ",array_registros[i].fecha_ejecucion);
+      printf("%s\n",array_registros[i].hora_ejecucion);
     }*/
 
-    ImprimirArchivoTemporal(record, numeroLineasArchivo, argv[1]);
+    ImprimirArchivoTemporal(array_registros, numero_lineasArchivo, argv[1]);
 
-    free(record);
+    free(array_registros);
 
     return 0;
 
 }
 
-int ContarLineasArchivo(char filename[]){
+int ContarLineasArchivo(char nombre_archivo[]){
     FILE *fp;
     int count = 0;  /* Line counter (result) */
-    /*char filename[MAX_FILE_NAME];*/
+    /*char nombre_archivo[MAX_FILE_NAME];*/
     char c;  /* To store a character read from file*/
 
     /* Open the file */
-    fp = fopen(filename, "r");
+    fp = fopen(nombre_archivo, "r");
 
     /* Check if file exists */
     if (fp == NULL){
-        perror ( filename ); /* why didn't the file open? */
+        perror ( nombre_archivo ); /* why didn't the file open? */
     }
 
     /* Extract characters from file and store in character c */
@@ -126,100 +126,100 @@ int ContarLineasArchivo(char filename[]){
 
     /* Close the file */
     fclose(fp);
-    /*printf("The file %s has %d lines\n ", filename, count);*/
+    /*printf("The file %s has %d lines\n ", nombre_archivo, count);*/
 
     return count;
 }
 
-void OrdenarRegistro(registro* record, int numeroElementos){
+void OrdenarRegistro(registro* array_registros, int numero_elementos){
 
     int i = 0, j = 0;
-    int resultadoComparacionStrings;
-    char fecha_hora_1[tamanoFilaFechaHora+tamanoFilaFechaHora+1];
-    char fecha_hora_2[tamanoFilaFechaHora+tamanoFilaFechaHora+1];
-    registro registroAuxiliar;
+    int resultado_comparacion_strings;
+    char fecha_hora_1[tamano_fila_fecha_hora+tamano_fila_fecha_hora+1];
+    char fecha_hora_2[tamano_fila_fecha_hora+tamano_fila_fecha_hora+1];
+    registro registro_auxiliar;
 
-    /*for (i = 0; i < numeroElementos; i++){
+    /*for (i = 0; i < numero_elementos; i++){
       printf("Contador:%d\n",i);
-      printf("%s",record[i].cadena);
-      printf("%d ",record[i].tiempoEjecucion);
-      printf("%s ",record[i].fechaEjecucion);
-      printf("%s\n",record[i].horaEjecucion);
+      printf("%s",array_registros[i].cadena);
+      printf("%d ",array_registros[i].tiempo_ejecucion);
+      printf("%s ",array_registros[i].fecha_ejecucion);
+      printf("%s\n",array_registros[i].hora_ejecucion);
     }*/
 
-    for (i = 0; i < numeroElementos; i++){
+    for (i = 0; i < numero_elementos; i++){
 
-      for (j = 0; j < numeroElementos-i-1; j++){
+      for (j = 0; j < numero_elementos-i-1; j++){
 
-        if(record[j].tiempoEjecucion > record[j+1].tiempoEjecucion){
+        if(array_registros[j].tiempo_ejecucion > array_registros[j+1].tiempo_ejecucion){
 
             /*Hay que intercabiar los elementos para que quede primero el menor*/
 
             /*Salvar los datos del que tiene menor tiempo de ejecución*/
-            strcpy(registroAuxiliar.cadena, record[j+1].cadena);
-            registroAuxiliar.tiempoEjecucion = record[j+1].tiempoEjecucion;
-            strcpy(registroAuxiliar.fechaEjecucion, record[j+1].fechaEjecucion);
-            strcpy(registroAuxiliar.horaEjecucion, record[j+1].horaEjecucion);
+            strcpy(registro_auxiliar.cadena, array_registros[j+1].cadena);
+            registro_auxiliar.tiempo_ejecucion = array_registros[j+1].tiempo_ejecucion;
+            strcpy(registro_auxiliar.fecha_ejecucion, array_registros[j+1].fecha_ejecucion);
+            strcpy(registro_auxiliar.hora_ejecucion, array_registros[j+1].hora_ejecucion);
 
             /*Cambiar elemento con menor tiempo por el otro*/
-            strcpy(record[j+1].cadena, record[j].cadena);
-            record[j+1].tiempoEjecucion = record[j].tiempoEjecucion;
-            strcpy(record[j+1].fechaEjecucion, record[j].fechaEjecucion);
-            strcpy(record[j+1].horaEjecucion, record[j].horaEjecucion);
+            strcpy(array_registros[j+1].cadena, array_registros[j].cadena);
+            array_registros[j+1].tiempo_ejecucion = array_registros[j].tiempo_ejecucion;
+            strcpy(array_registros[j+1].fecha_ejecucion, array_registros[j].fecha_ejecucion);
+            strcpy(array_registros[j+1].hora_ejecucion, array_registros[j].hora_ejecucion);
 
             /*Guardar los datos temporales para terminar intercambio*/
-            strcpy(record[j].cadena, registroAuxiliar.cadena);
-            record[j].tiempoEjecucion = registroAuxiliar.tiempoEjecucion;
-            strcpy(record[j].fechaEjecucion, registroAuxiliar.fechaEjecucion);
-            strcpy(record[j].horaEjecucion, registroAuxiliar.horaEjecucion);
+            strcpy(array_registros[j].cadena, registro_auxiliar.cadena);
+            array_registros[j].tiempo_ejecucion = registro_auxiliar.tiempo_ejecucion;
+            strcpy(array_registros[j].fecha_ejecucion, registro_auxiliar.fecha_ejecucion);
+            strcpy(array_registros[j].hora_ejecucion, registro_auxiliar.hora_ejecucion);
 
 
         }else{
-            /*if(record[i].tiempoEjecucion < record[i+1].tiempoEjecucion){*/
+            /*if(array_registros[i].tiempo_ejecucion < array_registros[i+1].tiempo_ejecucion){*/
 
               /*Hay que intercambiar los elementos para que quede primero */
 
             /*}else{*/
 
-            if(record[j].tiempoEjecucion == record[j+1].tiempoEjecucion){
+            if(array_registros[j].tiempo_ejecucion == array_registros[j+1].tiempo_ejecucion){
                 /*Si los tiempos de ejecución son iguales, entonces desempatar por lo siguiente*/
 
-                strcpy(fecha_hora_1, record[j].fechaEjecucion);
+                strcpy(fecha_hora_1, array_registros[j].fecha_ejecucion);
                 strcat(fecha_hora_1, " ");
-                strcat(fecha_hora_1, record[j].horaEjecucion);
+                strcat(fecha_hora_1, array_registros[j].hora_ejecucion);
                 /*printf("%s\n",fecha_hora_1);*/
 
-                strcpy(fecha_hora_2, record[j+1].fechaEjecucion);
+                strcpy(fecha_hora_2, array_registros[j+1].fecha_ejecucion);
                 strcat(fecha_hora_2, " ");
-                strcat(fecha_hora_2, record[j+1].horaEjecucion);
+                strcat(fecha_hora_2, array_registros[j+1].hora_ejecucion);
 
-                resultadoComparacionStrings = strcmp(fecha_hora_1, fecha_hora_2);
+                resultado_comparacion_strings = strcmp(fecha_hora_1, fecha_hora_2);
 
-                if( resultadoComparacionStrings > 0 ){
+                if( resultado_comparacion_strings > 0 ){
                   /*Si el elemento siguiente es mayor en fecha u hora*/
 
                   /*Hay que intercabiar los elementos para que quede primero el menor*/
 
                   /*Salvar los datos del que tiene menor tiempo de ejecución*/
-                  strcpy(registroAuxiliar.cadena, record[j+1].cadena);
-                  registroAuxiliar.tiempoEjecucion = record[j+1].tiempoEjecucion;
-                  strcpy(registroAuxiliar.fechaEjecucion, record[j+1].fechaEjecucion);
-                  strcpy(registroAuxiliar.horaEjecucion, record[j+1].horaEjecucion);
+                  strcpy(registro_auxiliar.cadena, array_registros[j+1].cadena);
+                  registro_auxiliar.tiempo_ejecucion = array_registros[j+1].tiempo_ejecucion;
+                  strcpy(registro_auxiliar.fecha_ejecucion, array_registros[j+1].fecha_ejecucion);
+                  strcpy(registro_auxiliar.hora_ejecucion, array_registros[j+1].hora_ejecucion);
 
                   /*Cambiar elemento con menor tiempo por el otro*/
-                  strcpy(record[j+1].cadena, record[j].cadena);
-                  record[j+1].tiempoEjecucion = record[j].tiempoEjecucion;
-                  strcpy(record[j+1].fechaEjecucion, record[j].fechaEjecucion);
-                  strcpy(record[j+1].horaEjecucion, record[j].horaEjecucion);
+                  strcpy(array_registros[j+1].cadena, array_registros[j].cadena);
+                  array_registros[j+1].tiempo_ejecucion = array_registros[j].tiempo_ejecucion;
+                  strcpy(array_registros[j+1].fecha_ejecucion, array_registros[j].fecha_ejecucion);
+                  strcpy(array_registros[j+1].hora_ejecucion, array_registros[j].hora_ejecucion);
 
                   /*Guardar los datos temporales para terminar intercambio*/
-                  strcpy(record[j].cadena, registroAuxiliar.cadena);
-                  record[j].tiempoEjecucion = registroAuxiliar.tiempoEjecucion;
-                  strcpy(record[j].fechaEjecucion, registroAuxiliar.fechaEjecucion);
-                  strcpy(record[j].horaEjecucion, registroAuxiliar.horaEjecucion);
+                  strcpy(array_registros[j].cadena, registro_auxiliar.cadena);
+                  array_registros[j].tiempo_ejecucion = registro_auxiliar.tiempo_ejecucion;
+                  strcpy(array_registros[j].fecha_ejecucion, registro_auxiliar.fecha_ejecucion);
+                  strcpy(array_registros[j].hora_ejecucion, registro_auxiliar.hora_ejecucion);
 
                 /*}else{
-                    if( resultadoComparacionStrings < 0 ){
+                    if( resultado_comparacion_strings < 0 ){
                 */
                 }
 
@@ -230,28 +230,28 @@ void OrdenarRegistro(registro* record, int numeroElementos){
       }
     }
 
-    /*for (i = 0; i < numeroElementos; i++){
+    /*for (i = 0; i < numero_elementos; i++){
       printf("Contador:%d\n",i);
-      printf("%s",record[i].cadena);
-      printf("%d ",record[i].tiempoEjecucion);
-      printf("%s ",record[i].fechaEjecucion);
-      printf("%s\n",record[i].horaEjecucion);
+      printf("%s",array_registros[i].cadena);
+      printf("%d ",array_registros[i].tiempo_ejecucion);
+      printf("%s ",array_registros[i].fecha_ejecucion);
+      printf("%s\n",array_registros[i].hora_ejecucion);
     }*/
 
 }
 
-void ImprimirArchivoTemporal(registro* record, int numeroElementos, char filename[]){
+void ImprimirArchivoTemporal(registro* array_registros, int numero_elementos, char nombre_archivo[]){
 
      int i = 0;
      FILE *fptr;
-     fptr = fopen(strcat(filename,"Temp"), "w");
+     fptr = fopen(strcat(nombre_archivo,"Temp"), "w");
      if(fptr == NULL){
         printf("Error!");
         exit(1);
      }
 
-     for (i = 0; i < numeroElementos; i++){
-          fprintf(fptr,"%s", record[i].cadena);
+     for (i = 0; i < numero_elementos; i++){
+          fprintf(fptr,"%s", array_registros[i].cadena);
      }
 
      fclose(fptr);
--- leerArchivo.h
+++ leerArchivo.h
@@ -1,15 +1,13 @@
-#define maxNombreArchivo 100
-#define maxNumeroRegistros 20000
-#define maxTamanoCadena 255
-#define tamanoFilaFechaHora 9
+#define max_tamano_cadena 255
+#define tamano_fila_fecha_hora 9
 
 typedef struct {
-  char cadena[maxTamanoCadena];
-  int tiempoEjecucion;
-  char fechaEjecucion[tamanoFilaFechaHora];
-  char horaEjecucion[tamanoFilaFechaHora];
+  char cadena[max_tamano_cadena];
+  int tiempo_ejecucion;
+  char fecha_ejecucion[tamano_fila_fecha_hora];
+  char hora_ejecucion[tamano_fila_fecha_hora];
 } registro ;
 
-int contarLineasArchivo(char filename[]);
-void ordenarRegistro(registro* record, int numeroElementos);
-void ImprimirArchivoTemporal(registro* record, int numeroElementos, char filename[]);
+int contarLineasArchivo(char nombre_archivo[]);
+void ordenarRegistro(registro* array_registros, int numero_elementos);
+void ImprimirArchivoTemporal(registro* array_registros, int numero_elementos, char nombre_archivo[]);
